c46a47dfd1be5c806ab77d0d47932880d8e16398
zhangliao <zhangliao@baiwang.com>
2020-04-30 12:26:09 +0800

    增加静态和动态页面示例

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, Render, Res} from '@nestjs/common';
 import { AppService } from './app.service';
+import {Response} from "express";
 
 @Controller()
 export class AppController {
@@ -8,5 +9,23 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('testtpl')
+  @Render('test')
+  getHelloTpl(){
+    return {
+      helloMsg: 'Hello Message, Hello Message'
+    }
+  }
+
+  @Get('testdtpl')
+  getHelloDynamicTpl(@Res() res: Response){
+    return res.render(
+        this.appService.getViewName(),
+        {
+          helloMsg: 'Hello Dynamic Message'
+        }
+    )
   }
 }
