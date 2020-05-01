62ebb7fe08ca5e82b14f4a068f851f1d0adaa5fc
zhangliao <zhangliao@baiwang.com>
2020-05-01 11:03:31 +0800

    增加配置文件读取

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
+import {join} from 'path'
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ServeStaticModule} from '@nestjs/serve-static'
-import {join} from 'path'
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
       ServeStaticModule.forRoot({
         rootPath:join(__dirname,'..','public')
+      }),
+      ConfigModule.forRoot({
+          envFilePath: `${join(__dirname,'..','config')}/.env.${process.env.NODE_ENV || 'development'}`,
+          isGlobal: true
       })
   ],
   controllers: [AppController],
